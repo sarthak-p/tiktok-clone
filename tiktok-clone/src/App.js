7f6ae9716f7a0b7342040b909d982e9f641e985f
Sarthak Patipati <pusarthak@gmail.com>
2021-08-03 18:50:34 -0500

    adding videos

--- tiktok-clone/src/App.js
+++ tiktok-clone/src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
+import "./App.css";
+import Video from "./Video";
 
 function App() {
   return (
     // BEM naming convention
     <div className="app">
       <h1>Let's build a full stack tiktok clone</h1>
+      <Video />
 
       {/* app-container */}
       {/* video */}
