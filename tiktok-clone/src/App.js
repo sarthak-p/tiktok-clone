b93df949566bd2b6c126de827b85c08efdcac91b
Sarthak Patipati <pusarthak@gmail.com>
2021-08-05 16:37:55 -0500

    added videos

--- tiktok-clone/src/App.js
+++ tiktok-clone/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react'; 
 import "./App.css";
 import Video from "./Video";
 
@@ -5,16 +6,13 @@ function App() {
   return (
     // BEM naming convention
     <div className="app">
-      <h1>Let's build a full stack tiktok clone</h1>
-      <Video />
-
-      {/* app-container */}
-      {/* video */}
-      {/* video */}
-      {/* video */}
-      {/* video */}
-      {/* video */}
-
+      <div className="app_videos">
+        <Video />
+        <Video />
+        <Video />
+        <Video />
+        <Video />
+        </div>
     </div>
   );
 }
--- tiktok-clone/src/Video.js
+++ tiktok-clone/src/Video.js
@@ -4,7 +4,8 @@ import './Video.css';
 function Video() {
   return (
       <div className="video">
-          <video src="https://youtu.be/E_yfA6PY284"></video>
+          <video className="video_player" loop controls src="https://v39-us.tiktokcdn.com/5f96cfc0db501428a2420299e6f1e63a/610ca77a/video/tos/useast2a/tos-useast2a-ve-0068c003/74b345bdeb3b4d95b4aa41304926d2e1/?a=1233&br=4228&bt=2114&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=Qq.UCEXk_4ka&l=20210805210729010190176022392F7C81&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M2o7OTQ6ZnY0NzMzNzczM0ApZWU5Omc2aDs5N2VmNTxoNmdxMTNhcjRvaS9gLS1kMTZzczYtMmJgNi1hL2IwMmEzMF86Yw%3D%3D&vl=&vr=">
+          </video>
     </div>
   );
 }
